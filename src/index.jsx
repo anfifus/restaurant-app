f18b5df5c5de4fffa9f60637cca9a53b9da2e220
anfifus <anfifus@gmail.com>
2023-02-03 21:03:23 +0100

    Feat: adding context

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,71 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/home/Home";
 import reportWebVitals from "./reportWebVitals";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { createContext, useState } from "react";
+const DEFAUT_THEME = "light";
+
+const getDesignPalette = (mode) => ({
+  mode,
+  ...(mode === DEFAUT_THEME
+    ? {
+        primary: {
+          main: "#fcfcfc",
+        },
+        secondary: {
+          main: "#FFFFFF",
+        },
+      }
+    : {
+        primary: {
+          main: "#fcfcfc",
+        },
+        secondary: {
+          main: "#FFFFFF",
+        },
+      }),
+});
+/* const themeDark = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fcfcfc",
+    },
+    secondary: {
+      main: "#FFFFFF",
+    },
+  },
+}); */
+export const ThemeContext = createContext({
+  theme: DEFAUT_THEME,
+  changeTheme: () => {},
+});
+
+function App() {
+  const [theme, setTheme] = useState(DEFAUT_THEME);
+  const changeTheme = () =>
+    setTheme(theme === DEFAUT_THEME ? "dark" : DEFAUT_THEME);
+  return (
+    <>
+      <ThemeContext.Provider
+        value={{
+          theme: theme,
+          setTheme: changeTheme,
+        }}
+      >
+        <ThemeProvider theme={createTheme(getDesignPalette(theme))}>
+          <Home />
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,15 +8,16 @@ import IntroRestaurant from "../../components/home/IntroRestaurante";
 import Contact from "../../components/home/Contacto";
 import FoodMenu from "../../components/home/Menu.Comida";
 import Fish from "../../components/core/Fish";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Restaurant from "../../components/home/Restaurant";
 import r1 from "../../components/Images/r-1.jpg";
 import r2 from "../../components/Images/r-2.jpg";
 import r3 from "../../components/Images/r-3.jpg";
 import r4 from "../../components/Images/r-4.jpg";
 import { createTheme } from "@mui/material";
-import { ThemeContext, ThemeProvider } from "@emotion/react";
+import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { ThemeContext } from "../..";
 
 const items = [
   { src: "http://restaurantleduc.com/wp-content/uploads/2015/03/2.jpg" },
@@ -27,19 +28,8 @@ const items = [
 ];
 const itemsCarousel2 = [{ src: r1 }, { src: r2 }, { src: r3 }, { src: r4 }];
 const res = 743 - 621;
-const themeDark = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#fcfcfc",
-    },
-    secondary: {
-      main: "#FFFFFF",
-    },
-  },
-});
 
-const themeLight = createTheme({
+/* const themeLight = createTheme({
   palette: {
     mode: "light",
     primary: {
@@ -49,93 +39,97 @@ const themeLight = createTheme({
       main: "#FFFFFF",
     },
   },
-});
+}); */
 
-const themeContext = React.createContext({
+/* const themeContext = React.createContext({
   theme: themeLight,
-  setTheme: () => {},
+  setTheme: () => {
+    const [theme, setTheme] = useState(themeLight);
+    setTheme(() => {
+      changeTheme(theme);
+    });
+  },
 });
+function changeTheme(theme) {
+  return theme === themeLight ? themeDark : themeLight;
+} */
 
 function App() {
   return (
-    <ThemeContext.Provider
-      value={{
-        theme: themeLight,
-        setTheme: () => {},
-      }}
-    >
-      <ThemeProvider theme={themeLight}>
-        <div className="App">
-          <header className="App-header">
-            <div>
-              <Header />
-            </div>
-          </header>
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <Header />
+        </div>
+      </header>
 
-          <section id="Carousel">
-            <Carousel
-              indicatorIconButtonProps={{
-                style: {
-                  bottom: { res },
-                  zIndex: "1",
-                },
-              }}
-            >
-              {items.map((item, index) => (
-                /* <ItemCarousel
+      <section id="Carousel">
+        <Carousel
+          indicatorIconButtonProps={{
+            style: {
+              bottom: { res },
+              zIndex: "1",
+            },
+          }}
+        >
+          {items.map((item, index) => (
+            /* <ItemCarousel
               key={"2015/03/" + index}
               item={item}
               className="images"
             /> */
-                <img
-                  key={"2015/03/" + index}
-                  src={item.src}
-                  alt={"2015/03/" + index}
-                  className="CarouselImage"
-                />
-              ))}
-            </Carousel>
-          </section>
-          <section id="introRestaurant">
-            <IntroRestaurant />
-          </section>
-          <section id="FoodMenu">
-            <Box maxWidth="100%">
-              <Fish />
-              <FoodMenu />
-            </Box>
-          </section>
-          <section id="restaurant">
-            <Restaurant />
-          </section>
-          <section id="carouselRestaurant">
-            <div>
-              <Carousel
-                indicatorIconButtonProps={{
-                  style: {
-                    bottom: { res },
-                    zIndex: "1",
-                  },
-                }}
-              >
-                {itemsCarousel2.map((item, index) => (
-                  /* <ItemCarousel key={"r-" + index} item={item} className="images" /> */
-                  <img
-                    key={"r-" + index}
-                    src={item.src}
-                    alt={"r-" + index}
-                    className="CarouselImage2"
-                  />
-                ))}
-              </Carousel>
-            </div>
-          </section>
-          <section id="Contact">
-            <Contact />
-          </section>
+            <img
+              key={"2015/03/" + index}
+              src={item.src}
+              alt={"2015/03/" + index}
+              className="CarouselImage"
+            />
+          ))}
+        </Carousel>
+      </section>
+      <section id="introRestaurant">
+        <IntroRestaurant />
+      </section>
+      <section id="FoodMenu">
+        <Box maxWidth="100%">
+          <Fish />
+          <FoodMenu />
+        </Box>
+      </section>
+      <section id="restaurant">
+        <Restaurant />
+      </section>
+      <section id="carouselRestaurant">
+        <div>
+          <Carousel
+            indicatorIconButtonProps={{
+              style: {
+                bottom: { res },
+                zIndex: "1",
+              },
+            }}
+          >
+            {itemsCarousel2.map((item, index) => (
+              /* <ItemCarousel key={"r-" + index} item={item} className="images" /> */
+              <img
+                key={"r-" + index}
+                src={item.src}
+                alt={"r-" + index}
+                className="CarouselImage2"
+              />
+            ))}
+          </Carousel>
         </div>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+      </section>
+      <section id="Contact">
+        <Contact />
+        <ThemeContext.Consumer>
+          {({ theme, changeTheme }) => (
+            <Button onClick={changeTheme}>{theme}</Button>
+          )}
+        </ThemeContext.Consumer>
+      </section>
+    </div>
   );
 }
 
